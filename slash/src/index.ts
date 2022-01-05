0b29740dbd2931760a38e6011aa783e2e0344eef
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-01-04 20:00:06 -0500

    Use node-fetch v2 to retain commonjs compatibility

--- slash/src/index.ts
+++ slash/src/index.ts
@@ -1,28 +1,22 @@
-import { Client, Intents } from "discord.js";
+import fastify from "fastify";
 import path from "path/posix";
-import { GatewayServer, SlashCreator } from "slash-create";
+import { FastifyServer, SlashCreator } from "slash-create";
 
-// Initialize Discord bot
-const client = new Client({
-  intents: Intents.FLAGS.GUILDS,
-});
+const app = fastify();
 
 // Initialize Slash creator (to handle interactions)
 const creator = new SlashCreator({
   applicationID: process.env.APPLICATION_ID!,
+  publicKey: process.env.PUBLIC_KEY,
   token: `Bot ${process.env.DISCORD_TOKEN}`,
 });
-
-creator
-  .withServer(new GatewayServer((h) => client.ws.on("INTERACTION_CREATE", h)))
-  .registerCommandsIn(path.join(__dirname, "commands"))
-  .syncCommands();
 
 // Log creator errors to console
 creator.on("warn", console.warn);
 creator.on("error", console.error);
 
-creator.on("synced", () => console.log("Synced all commands!"));
-client.on("ready", () => console.log("Discord client is ready!"));
-
-client.login(process.env.DISCORD_TOKEN);
+creator
+  .withServer(new FastifyServer(app))
+  .registerCommandsIn(path.join(__dirname, "commands"))
+  .syncCommands()
+  .startServer();
