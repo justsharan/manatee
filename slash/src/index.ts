788cb65ca56f859cd4afa45e192418b3269a288b
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-01-04 19:47:05 -0500

    Added comments

--- slash/src/index.ts
+++ slash/src/index.ts
@@ -2,10 +2,12 @@ import { Client, Intents } from "discord.js";
 import path from "path/posix";
 import { GatewayServer, SlashCreator } from "slash-create";
 
+// Initialize Discord bot
 const client = new Client({
   intents: Intents.FLAGS.GUILDS,
 });
 
+// Initialize Slash creator (to handle interactions)
 const creator = new SlashCreator({
   applicationID: process.env.APPLICATION_ID!,
   token: `Bot ${process.env.DISCORD_TOKEN}`,
