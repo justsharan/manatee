cd88b5f78b420a33bba6b9a9754ea7fec013a9e9
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-01-06 09:40:39 -0500

    Added utility class to build embed objects

---
+++ slash/src/structures/EmbedBuilder.ts
@@ -0,0 +1,62 @@
+import { MessageEmbedOptions } from "slash-create";
+
+export class EmbedBuilder {
+  constructor(private base: MessageEmbedOptions = {}) {}
+
+  title(title: string): EmbedBuilder {
+    this.base.title = title;
+    return this;
+  }
+
+  URL(url: string): EmbedBuilder {
+    this.base.url = url;
+    return this;
+  }
+
+  description(description: string): EmbedBuilder {
+    this.base.description = description;
+    return this;
+  }
+
+  color(color: number): EmbedBuilder {
+    this.base.color = color;
+    return this;
+  }
+
+  author(name: string, icon?: string, url?: string): EmbedBuilder {
+    this.base.author = { name, icon_url: icon, url };
+    return this;
+  }
+
+  timestamp(time: number | string | Date): EmbedBuilder {
+    this.base.timestamp = new Date(time).toISOString();
+    return this;
+  }
+
+  footer(text: string, icon: string): EmbedBuilder {
+    this.base.footer = { text, icon_url: icon };
+    return this;
+  }
+
+  field(name: string, value: string, inline = false): EmbedBuilder {
+    if (!this.base.fields || !this.base.fields.length) {
+      this.base.fields = [];
+    }
+    this.base.fields.push({ name, value, inline });
+    return this;
+  }
+
+  image(url: string): EmbedBuilder {
+    this.base.image = { url };
+    return this;
+  }
+
+  thumbnail(url: string): EmbedBuilder {
+    this.base.thumbnail = { url };
+    return this;
+  }
+
+  toJSON(): MessageEmbedOptions {
+    return this.base;
+  }
+}
