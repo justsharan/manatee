11061f7f19237bd520d80e18109fc01a2188ede1
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-01-13 09:47:54 -0800

    Optimize code for vercel

--- slash/api/interactions.ts
+++ slash/api/interactions.ts
@@ -1,3 +1,20 @@
-import { vercel } from "../index";
+import path from "path";
+import { SlashCreator, VercelServer } from "slash-create";
 
-export default vercel.vercelEndpoint;
+// Initialize Slash creator (to handle interactions)
+export const creator = new SlashCreator({
+  applicationID: process.env.APPLICATION_ID!,
+  publicKey: process.env.PUBLIC_KEY,
+  token: `Bot ${process.env.DISCORD_TOKEN}`,
+});
+
+// Log creator errors to console
+creator.on("warn", console.warn);
+creator.on("error", console.error);
+
+creator
+  .withServer(new VercelServer())
+  .registerCommandsIn(path.join(__dirname, "..", "commands"))
+  .syncCommands({ deleteCommands: true });
+
+export default (creator.server as VercelServer).vercelEndpoint;
