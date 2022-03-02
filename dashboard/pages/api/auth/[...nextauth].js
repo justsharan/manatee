d24386017cd5693142c2669080d35bc9ed7b88c0
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-02 14:44:55 -0800

    Pass in user ID

--- dashboard/pages/api/auth/[...nextauth].js
+++ dashboard/pages/api/auth/[...nextauth].js
@@ -13,4 +13,17 @@ export default NextAuth({
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.userId = token.sub;
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 });
