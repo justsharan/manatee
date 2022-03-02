177ad2b2eb958cd87d349631a4ca322b1937e3ca
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-01 23:10:11 -0800

    Added basic homepage

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,3 +1,23 @@
+import Navbar from "../components/Navbar";
+
+const dummyUser = {
+  avatar:
+    "https://cdn.discordapp.com/avatars/281193735788953601/5044b94e09b6262e1f5442d16dd53c1e.jpg?size=128",
+  name: "Cyborg#4239",
+};
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Navbar {...dummyUser} />
+      <main>
+        <section className="hero">
+          <span className="manatee">Manatee</span>
+          <p>
+            An awesome, easy-to-use Discord bot that's perfect for your server.
+          </p>
+        </section>
+      </main>
+    </>
+  );
 }
