9379b14f0600ae27872d9cf0f5e2c98c91616cf7
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-03 12:11:49 -0800

    Pass guild list in session

--- dashboard/pages/api/auth/[...nextauth].js
+++ dashboard/pages/api/auth/[...nextauth].js
@@ -18,11 +18,18 @@ export default NextAuth({
       if (account) {
         token.accessToken = account.access_token;
       }
+      if (!token.guilds) {
+        const res = await fetch("https://discord.com/api/v9/users/@me/guilds", {
+          headers: { Authorization: `Bearer ${token.accessToken}` },
+        });
+        token.guilds = await res.json();
+      }
       return token;
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) {
       session.userId = token.sub;
       session.accessToken = token.accessToken;
+      session.guilds = token.guilds;
       return session;
     },
   },
