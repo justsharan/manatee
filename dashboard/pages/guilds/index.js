92469891b1d5bcf2ecb8c8c94d67ea0299e806ed
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-03 12:56:02 -0800

    Move css to separate file

--- dashboard/pages/guilds/index.js
+++ dashboard/pages/guilds/index.js
@@ -3,27 +3,19 @@ import { useSession } from "next-auth/react";
 import Navbar from "../../components/Navbar";
 import GuildListItem from "../../components/GuildListItem";
 
+import styles from "./index.module.css";
+
 export default function Guilds() {
   const { data } = useSession();
   if (!data) return <>Loading</>;
   return (
     <>
-      <style jsx>{`
-        main {
-          display: flex;
-          flex-direction: row;
-          gap: 3em;
-          flex-wrap: wrap;
-          justify-content: center;
-          margin: 5em auto;
-        }
-      `}</style>
       <Navbar />
-      <header>
-        <h3>Welcome!</h3>
+      <header className={styles.hero}>
+        <h1>Welcome to Manatee!</h1>
         <p>Choose a server from below.</p>
       </header>
-      <main>
+      <main className={styles.list}>
         {data.guilds.map((guild) => (
           <GuildListItem {...guild} />
         ))}
