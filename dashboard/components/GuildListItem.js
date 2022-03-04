11319407e7b5da40f8f36d6d308ad91841f28330
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-03 17:43:09 -0800

    Added basic guild config page

--- dashboard/components/GuildListItem.js
+++ dashboard/components/GuildListItem.js
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import styles from "./GuildListItem.module.css";
 
 export default (props) => (
-  <section className={styles.card}>
-    <img
-      className={styles.icon}
-      src={`https://cdn.discordapp.com/icons/${props.id}/${props.icon}.jpg?size=256`}
-    />
-    <p>{condense(props.name)}</p>
-  </section>
+  <Link href={`/guilds/${props.id}`}>
+    <section className={styles.card}>
+      <img
+        className={styles.icon}
+        src={`https://cdn.discordapp.com/icons/${props.id}/${props.icon}.jpg?size=256`}
+      />
+      <p>{condense(props.name)}</p>
+    </section>
+  </Link>
 );
 
 const condense = (name) =>
