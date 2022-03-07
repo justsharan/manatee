a0137dc7c33229bdb4b8aeef1ea21643340c1e5f
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-06 17:45:14 -0800

    Send member log on member leaving

--- gateway/src/events/member.rs
+++ gateway/src/events/member.rs
@@ -1,7 +1,8 @@
 use crate::Context;
 use twilight_model::{
+  gateway::payload::incoming::MemberRemove,
   guild::Member,
-  id::Id
+  id::Id,
 };
 
 pub async fn guild_member_add(ctx: Context, member: Member) -> Result<(), sqlx::Error> {
@@ -37,6 +38,28 @@ pub async fn guild_member_add(ctx: Context, member: Member) -> Result<(), sqlx::
       member.user.id,
       Id::new(guilds[0].autorole.as_ref().unwrap().parse::<u64>().unwrap())
     ).exec().await.unwrap();
+  }
+
+  Ok(())
+}
+
+pub async fn guild_member_remove(ctx: Context, payload: MemberRemove) -> Result<(), sqlx::Error> {
+  let guilds = sqlx::query!("SELECT member_log FROM guilds WHERE id = $1;", payload.guild_id.get().to_string())
+    .fetch_all(&ctx.pool)
+    .await?;
+
+  // Send member log if enabled
+  if guilds[0].member_log.is_some() {
+    ctx.http.create_message(Id::new(guilds[0].member_log.as_ref().unwrap().parse::<u64>().unwrap()))
+      .content(
+        &format!(
+          "📤 **{}#{}** ({}) has left the server.\nAccount created: <t:{}>",
+          payload.user.name,
+          payload.user.discriminator().to_string(),
+          payload.user.id.get().to_string(),
+          ((payload.user.id.get() >> 22) + 1420070400000) / 1000
+        )
+      ).unwrap().exec().await.unwrap();
   }
 
   Ok(())
--- gateway/src/events/mod.rs
+++ gateway/src/events/mod.rs
@@ -9,6 +9,7 @@ pub async fn handle_events(ctx: Context, event: Event) -> Result<(), sqlx::Error
     Event::GuildCreate(guild_create) => guild::guild_create(ctx, guild_create.0).await,
     Event::GuildDelete(guild_delete) => guild::guild_delete(ctx, *guild_delete).await,
     Event::MemberAdd(member_add) => member::guild_member_add(ctx, member_add.0).await,
+    Event::MemberRemove(member_remove) => member::guild_member_remove(ctx, member_remove).await,
     _ => {
       println!("Unhandled event: {}", event.kind().name().unwrap_or("UNKNOWN"));
       Ok(())
