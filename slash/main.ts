656c0e5c40b6f6a9ea0253ebfc8dc12c1a93371b
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-08 12:09:43 -0800

    Finish porting over to CF Workers, mostly

--- slash/main.ts
+++ slash/main.ts
@@ -1,24 +1,10 @@
-import verify from "./verify";
+import commands from "./commands";
+import { SlashCreator, CFWorkerServer } from "./shim/";
 
-addEventListener("fetch", (event) => {
-  event.respondWith(handle(event.request, event.waitUntil.bind(event)));
+export const creator = new SlashCreator({
+  applicationID: DISCORD_APP_ID,
+  publicKey: DISCORD_PUBLIC_KEY,
+  token: DISCORD_BOT_TOKEN,
 });
 
-async function handle(req: Request, wait: (f: any) => void): Promise<Response> {
-  // Request data
-  const body = await req.text();
-  const signature = req.headers.get("X-Signature-Ed25519") ?? "";
-  const timestamp = req.headers.get("X-Signature-Timestamp") ?? "";
-
-  // Validate signature
-  const isValid = await verify(body, signature, timestamp);
-  if (!isValid) {
-    return new Response("Bad signature", { status: 401 });
-  }
-
-  const data = JSON.parse(body);
-  switch (data) {
-    default:
-      return new Response("Bad request", { status: 400 });
-  }
-}
+creator.withServer(new CFWorkerServer()).registerCommands(commands);
