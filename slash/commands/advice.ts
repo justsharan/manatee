aaf82ff2fb615c1b57d056792b035a95e964e6ac
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-27 14:16:21 -0700

    Use explicity type instead of casting

--- slash/commands/advice.ts
+++ slash/commands/advice.ts
@@ -2,6 +2,9 @@ import { CommandInteraction } from "discord-workers";
 
 export default async (int: CommandInteraction): Promise<Response> => {
   const res = await fetch("https://api.adviceslip.com/advice");
-  const body = (await res.json()) as any;
-  return int.respond(4, { content: body.slip.advice });
+  const body: any = await res.json();
+  return int.send({
+    content: body.slip.advice,
+    flags: 64,
+  });
 };
