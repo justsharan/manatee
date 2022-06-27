c1e2a3ecba4e9a2207ff70e6bc3bc840a819168a
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-06-27 15:28:43 -0700

    Added homepage with hero text

--- dashboard/pages/index.tsx
+++ dashboard/pages/index.tsx
@@ -1,10 +1,17 @@
 import { GetStaticPropsContext } from "next";
 import Navbar from "components/Navbar";
+import styles from "./index.module.css";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("Home");
   return (
     <>
       <Navbar />
+      <main className={styles.hero}>
+        <h1>Manatee</h1>
+        <p>{t("tagline")}</p>
+      </main>
     </>
   );
 }
