14ba1e26634013672cb689916c18f4e46e1a3f34
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-06-27 15:36:37 -0700

    Pass locale to button

--- dashboard/components/Button.tsx
+++ dashboard/components/Button.tsx
@@ -1,15 +1,19 @@
 import { MouseEventHandler, PropsWithChildren } from "react";
+import Link from "next/link";
 import styles from "./Button.module.css";
 
 type ButtonProps = {
   onClick?: MouseEventHandler<HTMLAnchorElement>;
   href?: string;
+  locale?: string;
 };
 
 export default function Button(props: PropsWithChildren<ButtonProps>) {
   return (
-    <a href={props.href} onClick={props.onClick} className={styles.btn}>
-      {props.children}
-    </a>
+    <div className={styles.btn}>
+      <Link href={props.href} onClick={props.onClick} locale={props.locale}>
+        {props.children}
+      </Link>
+    </div>
   );
 }
