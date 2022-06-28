78521a6f85fbecf1cf11050634d30fcb5aa7a5f5
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-06-27 17:02:11 -0700

    Use next redirects

--- dashboard/components/Layout.tsx
+++ dashboard/components/Layout.tsx
@@ -7,10 +7,6 @@ import Button from "components/Button";
 import styles from "./Layout.module.css";
 import Head from "next/head";
 
-const INVITE_LINK =
-  "https://discord.com/oauth2/authorize?scope=applications.commands%20bot&client_id=643103079679393792&permissions=164752486";
-const SUPPORT_LINK = "https://discord.gg/PMzqbaVEsH";
-
 export default function Layout(props: PropsWithChildren<{ title: string }>) {
   const t = useTranslations("Navbar");
   const { locale } = useRouter();
@@ -28,8 +24,8 @@ export default function Layout(props: PropsWithChildren<{ title: string }>) {
           <Link href="/docs" locale={locale}>
             {t("docs")}
           </Link>
-          <Link href={INVITE_LINK}>{t("invite")}</Link>
-          <Link href={SUPPORT_LINK}>{t("discord")}</Link>
+          <Link href="/invite">{t("invite")}</Link>
+          <Link href="/discord">{t("discord")}</Link>
         </div>
         <div>
           <Button href="/guilds" locale={locale}>
