dcdcc10e4de0ed56c46f1d51c557588effa75792
Sharan S <sharansr@pm.me>
2022-07-12 10:52:45 -0700

    Removed slash

--- slash/commands/cat.ts
+++
@@ -1,7 +0,0 @@
-import { CommandInteraction } from "discord-workers";
-
-export default async (int: CommandInteraction): Promise<Response> => {
-  const res = await fetch("https://api.thecatapi.com/v1/images/search");
-  const body: any = await res.json();
-  return int.send({ content: body[0].url });
-};
--- slash/types.d.ts
+++
@@ -1,7 +0,0 @@
-declare const DISCORD_APP_ID: string;
-declare const DISCORD_PUBLIC_KEY: string;
-declare const TMDB_KEY: string;
-declare const WEATHER_KEY: string;
-declare const MAPQUEST_KEY: string;
-declare const WEBHOOK_ID: string;
-declare const WEBHOOK_TOKEN: string;
